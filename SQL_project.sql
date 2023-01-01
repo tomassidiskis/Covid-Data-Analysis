72d3bdd7d8a47f0b7d483d9d09da08bf9efa20ad
Tomas <102066899+tomassidiskis@users.noreply.github.com>
2023-01-01 15:15:39 +0000

    Update SQL_project.sql

--- SQL_project.sql
+++ SQL_project.sql
@@ -1,6 +1,3 @@
--- Tableau visualization of this project:
--- https://public.tableau.com/app/profile/tomassidiskis/viz/CovidDashboardProject_16505651008590/Dashboard1
-
 SELECT *
 FROM PortfolioProject..CovidDeaths
 WHERE continent is not null
